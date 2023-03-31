a6bd28563e18ef26ded8bacd12f44bb4bf9553ad
Gustavo Satig <gustavosatig@ppi-multitask.com.br>
2023-03-31 18:59:21 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace GradilSolutions
+{
+    partial class GradilSolutions
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GradilSolutions));
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.generateGradil = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConfirmar
+            // 
+            resources.ApplyResources(this.btnConfirmar, "btnConfirmar");
+            this.btnConfirmar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnConfirmar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            this.btnConfirmar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnCancelar
+            // 
+            resources.ApplyResources(this.btnCancelar, "btnCancelar");
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // generateGradil
+            // 
+            resources.ApplyResources(this.generateGradil, "generateGradil");
+            this.generateGradil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.generateGradil.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.generateGradil.Name = "generateGradil";
+            this.generateGradil.UseVisualStyleBackColor = false;
+            this.generateGradil.Click += new System.EventHandler(this.generateGradil_Click);
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // GradilSolutions
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.generateGradil);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnConfirmar);
+            this.Name = "GradilSolutions";
+            this.Load += new System.EventHandler(this.GradilSolutions_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button generateGradil;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradilSolutions
+{
+    public partial class GradilSolutions : Form
+    {
+        public GradilSolutions()
+        {
+            InitializeComponent();
+
+            // Adicionar colunas a tela
+            dataGridView1.Columns.Add("Produto", "Nome do Produto");
+            dataGridView1.Columns.Add("Quantidade", "Quantidade do Produto");
+
+            // Definir tamanho das células
+            dataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.Columns["Produto"].Width = 150;
+            dataGridView1.Columns["Quantidade"].Width = 175;
+            dataGridView1.Columns["Produto"].DefaultCellStyle.BackColor = Color.LightBlue;
+            dataGridView1.Columns["Quantidade"].DefaultCellStyle.BackColor = Color.LightCyan;
+
+            // Adiciona os tamanhos de altura da cerca
+            comboBox1.Items.Add("1,03");
+            comboBox1.Items.Add("1,53");
+            comboBox1.Items.Add("2,03");
+
+            // Adiciona as cores de acordo com as especificadas
+            comboBox2.Items.Add("Sem pintura");
+            comboBox2.Items.Add("Branca");
+            comboBox2.Items.Add("Preta");
+            comboBox2.Items.Add("Verde");
+
+            // Define o valor padrão da cor
+            comboBox2.SelectedIndex = 0;
+
+            // Define o valor padrão do comprimento da cerca
+            textBox1.Text = "0";
+        }
+
+        //botao de confirmar que deve se conectar com o datagridview
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Verificar se o usuário selecionou opções válidas
+            if (string.IsNullOrEmpty(textBox1.Text) || comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, selecione todas as opções antes de confirmar.");
+                return;
+            }
+
+            // Obter comprimento total da cerca
+            if (!double.TryParse(textBox1.Text, out double comprimento))
+            {
+                MessageBox.Show("Por favor, insira um comprimento válido.");
+                return;
+            }
+
+            // Obter altura da cerca
+            double altura;
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "1,03":
+                    altura = 1.03;
+                    break;
+                case "1,53":
+                    altura = 1.53;
+                    break;
+                case "2,03":
+                    altura = 2.03;
+                    break;
+                default:
+                    MessageBox.Show("Por favor, selecione uma altura válida.");
+                    return;
+            }
+
+            // Obter cor da cerca
+            string cor = comboBox2.SelectedItem.ToString();
+
+            // Realizar os cálculos
+            int quantidade_telas = (int)Math.Ceiling(comprimento / 2.5);
+            int quantidade_postes = quantidade_telas * 2;
+            int quantidade_fixadores = 0;
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "1,03":
+                    quantidade_fixadores = quantidade_telas * 3;
+                    break;
+                case "1,53":
+                    quantidade_fixadores = quantidade_telas * 4;
+                    break;
+                case "2,03":
+                    quantidade_fixadores = quantidade_telas * 6;
+                    break;
+            }
+            int quantidade_parafusos = quantidade_postes * 4;
+
+            double diferenca;
+            diferenca = (quantidade_telas * 2.5) - comprimento;
+
+            // Mostrar os resultados na tela
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add("Telas:", quantidade_telas);
+            dataGridView1.Rows.Add("Postes:", quantidade_postes);
+            dataGridView1.Rows.Add("Fixadores:", quantidade_fixadores);
+            dataGridView1.Rows.Add("Parafusos:", quantidade_parafusos);
+            dataGridView1.Rows.Add("Cor da Tela:", cor);
+            dataGridView1.Rows.Add("Diferença:", diferenca);
+        }
+        //altura da cerca
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+        //cor da cerca
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //Comprimento total da cerca em metros
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+        //apenas uma label
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Aqui preciso mostrar os resultados dos calculos 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        //apenas uma label
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GradilSolutions_Load(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void generateGradil_Click(object sender, EventArgs e)
+        {
+    // Obter as especificações da cerca do usuário
+    double altura = double.Parse(txtAltura.Text);
+    double largura = double.Parse(txtLargura.Text);
+
+    // Calcular o comprimento da cerca
+    double comprimento = 2 * altura + 2 * largura;
+
+    // Gerar o código HTML para o gráfico
+    string html = "<html><head><script src=\"https://cdn.jsdelivr.net/npm/chart.js\"></script></head><body>";
+    html += "<canvas id=\"myChart\"></canvas>";
+    html += "<script>var ctx = document.getElementById('myChart').getContext('2d');";
+    html += "var myChart = new Chart(ctx, {type: 'bar',data: {labels: ['Comprimento'],datasets: [{label: 'Comprimento da cerca',data: [" + comprimento + "],backgroundColor: ['rgba(255, 99, 132, 0.2)'],borderColor: ['rgba(255, 99, 132, 1)'],borderWidth: 1}]}});</script>";
+    html += "</body></html>";
+
+    // Exibir o gráfico no WebBrowser
+    webBrowser.DocumentText = html;
+}
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
